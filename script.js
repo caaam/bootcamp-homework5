f0e383271d769fd2120bee8f40128634b54f4dcf
cam <cam@camherbert.com>
2021-09-07 21:37:18 -0400

    added calendar blocks

--- script.js
+++ script.js
@@ -9,22 +9,26 @@ for (var i = 8; i < 12; i++) {
 
 testHourPm(12);
 
-for (var i = 1; i < 10; i++) {
+for (var i = 1; i < 12; i++) {
     testHourPm(i);
 }
 
 function testHourAm(hour) {
     if (moment("" + hour + ":00 am", "h:mm a").fromNow().includes("minutes ago")) {
         console.log("the " + hour + " o'clock hour is now");
-        $(".container").append("<h3>The " + hour + "am hour is now</h3>");
+        console.log("#" + hour + "am-content");
+        $("#" + hour + "am-content").addClass("bg-danger text-dark");
+        // $(".container").append("<h3>The " + hour + "am hour is now</h3>");
         // console.log(moment("" + hour + ":00 am", "h:mm a").fromNow());
     } else if (moment("" + hour + ":00 am", "h:mm a").fromNow().includes("ago")) {
         console.log(hour + " is earlier");
-        $(".container").append("<h3>" + hour + "am is earlier</h3>");
+        $("#" + hour + "am-content").addClass("bg-secondary text-dark");
+        // $(".container").append("<h3>" + hour + "am is earlier</h3>");
         // console.log(moment("" + hour + ":00 am", "h:mm a").fromNow());
     } else if (moment("" + hour + ":00 am", "h:mm a").fromNow().includes("in ")) {
         console.log(hour + " is later");
-        $(".container").append("<h3>" + hour + "am is later</h3>");
+        $("#" + hour + "am-content").addClass("bg-success text-dark");
+        // $(".container").append("<h3>" + hour + "am is later</h3>");
         // console.log(moment("" + hour + ":00 am", "h:mm a").fromNow());
     }
 }
@@ -32,15 +36,19 @@ function testHourAm(hour) {
 function testHourPm(hour) {
     if (moment("" + hour + ":00 pm", "h:mm a").fromNow().includes("minutes ago")) {
         console.log("the " + hour + " o'clock hour is now");
-        $(".container").append("<h3>The " + hour + "pm hour is now</h3>");
+        console.log("#" + hour + "pm-content");
+        $("#" + hour + "pm-content").addClass("bg-danger text-dark");
+        // $(".container").append("<h3>The " + hour + "pm hour is now</h3>");
         // console.log(moment("" + hour + ":00 am", "h:mm a").fromNow());
     } else if (moment("" + hour + ":00 pm", "h:mm a").fromNow().includes("ago")) {
         console.log(hour + " is earlier");
-        $(".container").append("<h3>" + hour + "pm is earlier</h3>");
-        console.log(moment("" + hour + ":00 pm", "h:mm a").fromNow());
+        $("#" + hour + "pm-content").addClass("bg-secondary text-dark");
+        // $(".container").append("<h3>" + hour + "pm is earlier</h3>");
+        // console.log(moment("" + hour + ":00 pm", "h:mm a").fromNow());
     } else if (moment("" + hour + ":00 pm", "h:mm a").fromNow().includes("in ")) {
         console.log(hour + " is later");
-        $(".container").append("<h3>" + hour + "pm is later</h3>");
+        $("#" + hour + "pm-content").addClass("bg-success text-dark");
+        // $(".container").append("<h3>" + hour + "pm is later</h3>");
         // console.log(moment("" + hour + ":00 pm", "h:mm a").fromNow());
     }
 }
